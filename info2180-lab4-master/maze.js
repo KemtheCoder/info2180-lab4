e4467c8e9d4820d0c45ab52506edf7a8fbf750e4
Oliver Morgan <Oliver Morgan>
2016-10-14 18:31:21 -0400

    redo

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,30 +1,12 @@
-var loser = false;
+//#1
 
 window.onload = function(){
-  $("start").onclick = startClick;
-  $("end").onmouseover = overEnd;
-  var boundaries = $$("div#maze div.boundary");
-  for (var i = 0; i<boundaries.length; i++){
-    boundaries[i].onmouseover = overBoundary;
+  var count=0;
+  function change(){
+    document.getElementById("boundary").className +="youlose";
+    count+=1;
   }
 }
 
-function overBoundary(){
-  loser = true;
-  $("status").textContent = "You lose!";
-  var boundaries = $$("div#maze div.boundary");
-  for (var i = 0; i < boundaries.length; i++){
-    boundaries[i].addClassName("youlose");
-    }
-}
-
-function startClick(){
-  loser = false;
-  $("status").textContent = "Find the end!";
-  var boundaries = $$("div#maze div.boundary");
-  for (var i = 0; i < boundaries.length; i++){
-    boundaries[i].removeClassName("youlose");
-  }
-}
     
 
